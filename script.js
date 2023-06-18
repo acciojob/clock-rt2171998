4c4d0d116c1405277e11538681668b6223d2c7bb
rt2171998 <70574052+rt2171998@users.noreply.github.com>
2023-06-18 14:58:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 //your JS code here. If required.
+function updateDateTime() {
+      var currentDate = new Date();
+      var year = currentDate.getFullYear();
+      var month = (currentDate.getMonth() + 1).toString().padStart(2, "0");
+      var day = currentDate.getDate().toString().padStart(2, "0");
+      var hours = currentDate.getHours().toString().padStart(2, "0");
+      var minutes = currentDate.getMinutes().toString().padStart(2, "0");
+      var seconds = currentDate.getSeconds().toString().padStart(2, "0");
+
+      var formattedDate = day + "/" + month + "/" + year;
+      var formattedTime = hours + ":" + minutes + ":" + seconds;
+
+      document.getElementById("timer").textContent = formattedDate+" "+formattedTime;
+    }
+setInterval(updateDateTime, 1000);
